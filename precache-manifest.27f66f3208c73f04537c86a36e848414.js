ac0bd7acf80eb9f65a9fce93116096b8f15865e5
Michael Priest <michael-priest@hotmail.com>
2020-01-13 22:12:08 +0000

    Updates

---
+++ precache-manifest.27f66f3208c73f04537c86a36e848414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0c70ffc8d4ac26bd28875f794ddfe5",
+    "url": "/React-Particle-Backgrounds-Demo/index.html"
+  },
+  {
+    "revision": "2bf2fc499ddb794ac93e",
+    "url": "/React-Particle-Backgrounds-Demo/static/css/main.fcb7666e.chunk.css"
+  },
+  {
+    "revision": "5dbaeb6ff475769e1020",
+    "url": "/React-Particle-Backgrounds-Demo/static/js/2.686851f0.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "/React-Particle-Backgrounds-Demo/static/js/2.686851f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2bf2fc499ddb794ac93e",
+    "url": "/React-Particle-Backgrounds-Demo/static/js/main.ebb4265a.chunk.js"
+  },
+  {
+    "revision": "1e47cb3de8641ec8e9d8",
+    "url": "/React-Particle-Backgrounds-Demo/static/js/runtime-main.83824b7f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Particle-Backgrounds-Demo/precache-manifest.346459597fe432403f07e095d0b79622.js"
+  "/React-Particle-Backgrounds-Demo/precache-manifest.27f66f3208c73f04537c86a36e848414.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Particle-Backgrounds-Demo/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
